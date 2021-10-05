ca673affe20202b75b16cc54ad0f7271cdd98907
Jayden Lefebvre <jayden.lefebvre55@gmail.com>
2021-10-05 16:30:40 -0400

    Lab 2 Setup

---
+++ lab2/GameState.cpp
@@ -0,0 +1,73 @@
+//
+//  GameState.cpp
+//  TicTacToe
+//
+//  Created by Tarek Abdelrahman on 2019-06-07.
+//  Modified by Tarek Abdelrahman on 2020-09-17.
+//  Copyright © 2019-2020 Tarek Abdelrahman. All rights reserved.
+//
+//  Permission is hereby granted to use this code in ECE244 at
+//  the University of Toronto. It is prohibited to distribute
+//  this code, either publicly or to third parties.
+//
+
+#include "globals.h"
+#include "GameState.h"
+
+// ECE244 Student: add you code below
+
+int GameState::get_selectedRow(){
+
+}
+
+int GameState::get_selectedColumn(){
+
+}
+
+void GameState::set_selectedRow(int value){
+
+}
+
+void GameState::set_selectedColumn(int value){
+
+}
+
+bool GameState::get_moveValid(){
+
+}
+
+void GameState::set_moveValid(bool value){
+
+}
+
+bool GameState::get_gameOver(){
+
+}
+
+void GameState::set_gameOver(bool value){
+
+}
+
+int GameState::get_winCode(){
+
+}
+
+void GameState::set_winCode(int value){
+
+}
+
+bool GameState::get_turn(){
+    
+}
+
+void GameState::set_turn(bool value){
+
+}
+
+int GameState::get_gameBoard(int row, int col){
+
+}
+
+void GameState::set_gameBoard(int row, int col, int value){
+    
+}
